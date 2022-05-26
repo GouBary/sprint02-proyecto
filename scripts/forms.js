f2651beb8cba89654713fb2fd68b4e5c68403030
Juan Carlos Morales Huertas <101436132+GouBary@users.noreply.github.com>
2022-05-26 13:09:45 -0500

    Add files via upload

---
+++ scripts/forms.js
@@ -0,0 +1,42 @@
+const form = document.getElementById("formulario")
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+    const uno = document.getElementById("categoria").value;
+    const dos = document.getElementById("video").value;
+    const tres = document.getElementById("usuario").value;
+    const cuatro = document.getElementById("titulo").value;
+    const cinco = document.getElementById("canal").value;
+    const seis = document.getElementById("vistas").value;
+    const siete = document.getElementById("fecha").value;
+
+    if(uno === '' || dos === '' || tres === '' || cuatro === '' || cinco === '' || seis === '' || siete ===''){
+    Swal.fire(
+        'Completa los espacios faltantes',
+        'You clicked the button!',
+        'error'
+      )}
+      else{
+        Swal.fire(
+            'Video agregado',
+            'You clicked the button!',
+            'success'
+      )}
+    const llenar = {
+        id: 31,
+        inicio: "todo" ,
+        categoria: uno ,
+        usuario:dos ,
+        icono:"../iconos/charli.jpg",
+        nombre:cuatro,
+        canal:cinco,
+        vistas:seis,
+        fecha:siete,
+    }
+    localStorage.setItem('insertar', JSON.stringify(llenar)) 
+
+    const botonBorrar = document.getElementById('boton');
+       botonBorrar.addEventListener('click', function(){
+       location.reload();})
+
+        }
